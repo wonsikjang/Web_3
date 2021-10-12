dfa01e9bf5b82586ce8d3cfd410bb8cacf04d287
jia <abell2667@gm.gist.ac.kr>
2021-10-13 01:31:02 +0900

    #0.1 React + Firebase Setup_JA

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrGjq3VMlPj1Dohj6VgRlhQjsOl90FGHk",
+    authDomain: "nwitter-24ca9.firebaseapp.com",
+    projectId: "nwitter-24ca9",
+    storageBucket: "nwitter-24ca9.appspot.com",
+    messagingSenderId: "893538047029",
+    appId: "1:893538047029:web:713237282180903dd98d7c"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
