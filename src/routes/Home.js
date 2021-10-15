e1a0b8cf4540753171d27344e0e06fc93b4db343
장원식 <arkimas82@gmail.com>
2021-10-15 15:20:14 +0900

    3.2

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { dbService } from "fbase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection,getDocs, query } from "firebase/firestore";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    
+    const getNweets = async () => {
+      const q = query(collection(dbService, "nweets"));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+      const nweetObj = {
+      ...doc.data(),
+      id: doc.id,
+      }
+      setNweets(prev => [nweetObj, ...prev]);
+      });
+      };
+      useEffect(() => {
+      getNweets();
+      }, []);
     const onSubmit = async(e) => {
       e.preventDefault();
       try {
@@ -24,6 +40,7 @@ const Home = () => {
       } = event;
       setNweet(value);
     };
+    console.log(nweets);
     return (
       <div>
         <form onSubmit={onSubmit}>
@@ -36,6 +53,13 @@ const Home = () => {
           />
           <input type="submit" value="Nweet" />
         </form>
+        <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
       </div>
     );
   };
