6b5a99812b91ed4f50314144dc14aa39ce73415f
장원식 <arkimas82@gmail.com>
2021-10-17 01:26:22 +0900

    4.1

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,7 @@ import Nweet from "components/Nweet";
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
     useEffect( () => {
         onSnapshot (
         query(collection(dbService, "nweets"), orderBy("createdAt", "desc")),
@@ -39,10 +40,15 @@ const Home = ({ userObj }) => {
         const theFile = files[0];
         const reader = new FileReader();
         reader.onloadend = (finishedEvent) => {
-          console.log(finishedEvent);
+            const {
+                currentTarget: { result },
+            } = finishedEvent;
+            setAttachment(result);
         };
+        
         reader.readAsDataURL(theFile);
       };
+      const onClearAttachment = () => setAttachment(null);
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -55,6 +61,13 @@ const Home = ({ userObj }) => {
                 />
                  <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Nweet" />
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px" />
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>
+                )}
+            
             </form>
             <div>
                 {nweets.map((nweet) => (
